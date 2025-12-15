8f01f10976bab6daca887a3feea4e928efa6cc07
Michael Wu <mw10013@gmail.com>
2025-12-15 10:52:24 -0500

    feat(ui): move NotFound component to lowercase filename and update import path

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,7 @@ import { createRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
-import { NotFound } from './components/NotFound'
+import { NotFound } from './components/not-found'
 
 // Create a new router instance
 export const getRouter = () => {
